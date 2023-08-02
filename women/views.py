6f466b9f434564808f4cb20f6fa6846b870b3576
Artem Belskiy <lamba777a@gmail.com>
2023-08-02 20:59:33 +0200

    Added int(slug), and second views category

--- women/views.py
+++ women/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('<h1>Страница Django</h1>')
+
+def categories(request, catid):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
