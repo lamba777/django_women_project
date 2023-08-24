5b2858e0ac423a6af18ecaec4b07c1a58d587a3f
Artem Belskiy <lamba777a@gmail.com>
2023-08-24 12:22:43 +0200

    form converted to bound to class. Added custom validator for title field

--- women/views.py
+++ women/views.py
@@ -21,11 +21,11 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
             try:
-                Women.objects.create(**form.cleaned_data)
+                form.save()
                 return redirect('home')
             except:
                 form.add_error(None, 'Ошибка добавления поста')
